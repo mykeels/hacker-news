1bcf5d463038a80bb8c91048ddea1bdc1e0684a9
mykeels <mykehell123@gmail.com>
2016-12-01 23:11:19 +0100

    added HTTPS made angular load from local cos of http certificate conflict

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 var app = angular.module("HackerNewsApp", []);
 
 app.controller("ListingCtrl", function ($scope, $http) {
-    $scope.news = new HackerNews();
+    $scope.news = new HackerNews($http);
     $scope.news.getTopStories();
+});
+
+app.directive("lazyLoad", function ($window) {
+    return {
+        restrict: 'A',
+        link: function ($scope, element, attrs) {
+            var $w = angular.element($window);
+            $w.on("scroll", function (ev) {
+                //console.log(document.documentElement.scrollHeight, window.innerHeight, window.pageYOffset);
+                if ((document.documentElement.scrollHeight - window.innerHeight) == window.pageYOffset) {
+                    //console.log(ev);
+                    $scope.news.showNext();
+                }
+            });
+        }
+    }
 });
--- hacker-news.js
+++ hacker-news.js
@@ -1,38 +1,32 @@
-function HackerNews() {
+function HackerNews($http) {
     this.stories = [];
-    this.stories.limit = 0;
+    this.limit = 0;
     var me = this;
     this.getTopStories = function () {
-        return fetch("https://hacker-news.firebaseio.com/v0/topstories.json")
-                .then(function (storyIds) {
-                    return storyIds.json();
-                })
-                .then(function (storyIds) {
+        return $http.get("https://hacker-news.firebaseio.com/v0/topstories.json")
+                .success(function (storyIds) {
                     console.log(storyIds);
-                    me.stories = storyIds.map(function (id) {
+                    me.stories = (storyIds.map(function (id) {
                         return { id: id }
-                    });
+                    }));
                     me.showNext();
                 })
-                .catch(function (err, obj) {
+                .error(function (err, obj) {
                     console.error(err, obj);
                 });
     }
     this.getValidStories = function () {
-        return me.stories.slice(0, me.stories.limit);
+        return me.stories.slice(0, me.limit);
     }
     this.showNext = function () {
-        for (var i = limit; i < Math.min(me.stories.length, (limit + 10)); i++) {
+        for (var i = me.limit; i < Math.min(me.stories.length, (me.limit + 10)) ; i++) {
             me.getStory(me.stories[i].id);
         }
-        limit += 10;
+        me.limit += 10;
     }
     this.getStory = function (storyId) {
-        return fetch("https://hacker-news.firebaseio.com/v0/item/" + storyId + ".json")
-                .then(function (story) {
-                    return story.json();
-                })
-                .then(function (story) {
+        return $http.get("https://hacker-news.firebaseio.com/v0/item/" + storyId + ".json")
+                .success(function (story) {
                     console.log(story);
                     var storyObj = me.stories.filter(function (st) {
                         return st.id == storyId;
@@ -43,7 +37,7 @@ function HackerNews() {
                         }
                     }
                 })
-                .catch(function (err, obj) {
+                .error(function (err, obj) {
                     console.error(err, obj);
                 });
     }
